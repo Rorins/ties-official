2680a65969cdcaaf417e237264d4d0796d88439c
Aurora <aurora.grippaudo@dotenv.it>
2023-03-17 23:50:44 +0100

    added element at end of page

---
+++ storyblok/CenteredColumn.vue
@@ -0,0 +1,23 @@
+<template>
+    <div v-editable="blok">
+    <div class="container">
+        <img :src=blok.image.filename :alt="blok.headline" />
+        <p class="text-lg">{{ blok.text}}</p>
+    </div>
+    </div>
+  </template>
+     
+  <script setup>
+    defineProps({ blok: Object })
+  </script>
+  
+  
+  <style scoped lang='scss'>
+  .container{
+    text-align:center;
+    padding:80px 0;
+    img{
+    width: 200px;
+  }
+  }
+  </style>
