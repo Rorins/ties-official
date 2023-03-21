5b66dab48f65a63b6aeea1fab0422a4f30d0f8cd
Aurora <aurora.grippaudo@dotenv.it>
2023-03-21 16:32:21 +0100

    Added listeners to listeners page

---
+++ storyblok/Users.vue
@@ -0,0 +1,24 @@
+<template>
+    <section v-editable="blok" class="users bg_colordark">
+    <div class="container">
+    <div class="row">
+      <ListenerCard 
+      v-for="user in blok.users"
+      class="col-12 col-lg-6"
+          :key="user._uid"
+          :img="user.img.filename"
+          :name="user.name"
+          :quote="user.quote"
+          :description="user.description"
+        />
+    </div>
+      </div>
+    </section>
+  </template>
+
+<script setup>
+defineProps({ blok: Object });
+</script>
+
+<style scoped lang="scss">
+</style>
