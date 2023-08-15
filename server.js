d416c5e8ea8f79c034f41b9045122e3edc5215ef
Rorins <aurora268@hotmail.it>
2023-08-14 21:24:48 -0500

    added json for vercel

--- server.js
+++ server.js
@@ -9,13 +9,10 @@ const app = express();
 app.use(cors());
 
 
-
 // Start the server
 app.use(bodyParser.json());
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
-
-
 
