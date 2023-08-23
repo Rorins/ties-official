b7c4297fa1512d494d01ec6a71dc2f0d371fab0f
Rorins <aurora268@hotmail.it>
2023-08-23 19:47:40 +0200

    reinstalled pupeteer stealth

--- api/authenticate.js
+++ api/authenticate.js
@@ -12,7 +12,7 @@ async function authenticateCharacterAI() {
         } else {
           await characterAI.authenticateWithToken(userToken);
           console.log("Character AI authenticated with token.");
-        }ac
+        }
       } catch (error) {
         console.error("Error authenticating Character AI:", error);
         process.exit(1); // Exit the server if authentication fails
